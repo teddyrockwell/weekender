126e126636aaa8a945d743dd50802e62a2079f55
bsegelke <bbsegelke@gmail.com>
2023-03-25 10:07:36 -0400

    ADD mongoose databases and schemas and models

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require ('path');
+const { Users, Trips, Weather } = require('./db/index')
 const clientPath = path.resolve(__dirname, '..', 'dist');
 const port = 8080;
 
